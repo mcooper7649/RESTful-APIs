c6b660de8afcfd196eaa73f209726fefb79b35e4
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-04-07 10:24:28 -0400

    Completed setup of CodeWiki API

---
+++ Wiki-API/app.js
@@ -0,0 +1,42 @@
+//jshint esversion:6
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+mongoose.connect('mongodb://localhost:27017/wikiDB', {useNewUrlParser: true, useUnifiedTopology: true })
+
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+// app.get("/", function(req, res ){
+//     Article.find({}, function (err, foundItems) {
+//         console.log(foundItems)
+//     })
+// }
+
+let port = process.env.PORT;
+if(port == null || port == ""){
+    port = 3000;
+}
+
+
+
+app.listen(port, function(){
+    console.log("Server started Successfully");
+})
